6d49375c02e77c06bf102de7002dbb5bdea6aff9
Davin Higo <davinhigo@gmail.com>
2023-11-16 13:59:32 -0800

    made search.js for search page

---
+++ scripts/search.js
@@ -0,0 +1,64 @@
+//Global variable pointing to the current user's Firestore document
+var currentUser;   
+
+//Function that calls everything needed for the main page  
+function doAll() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid); //global
+            console.log(currentUser);
+
+            displayCardsDynamically("posts");
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
+            window.location.href = "login.html";
+        }
+    });
+}
+doAll();   
+
+//------------------------------------------------------------------------------
+// Input parameter is a string representing the collection we are reading from
+//------------------------------------------------------------------------------
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("hikeCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+
+    db.collection(collection)   //the collection called "hikes"
+        .limit(2)
+        .get()
+        .then(allHikes=> {
+            //var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allHikes.forEach(doc => { //iterate thru each doc
+                var poster = doc.data().posterName; 
+                var from = doc.data().from;  // get value of the "details" key
+				var to = doc.data().time;    //get unique ID to each hike to be used for fetching right image
+                var hikeLength = doc.data().price; //gets the length field
+                var docID = doc.id;
+                let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = poster;
+                newcard.querySelector('.card-length').innerHTML = to;
+                newcard.querySelector('.card-text').innerHTML = from;
+                newcard.querySelector('.card-text2').innerHTML = to;
+                newcard.querySelector('a').href = "eachHike.html?docID="+docID;
+                newcard.querySelector('i').id = 'save-' + docID;   //guaranteed to be unique
+                newcard.querySelector('i').onclick = () => saveBookmark(docID);
+                newcard.querySelector('.card-length').innerHTML =
+                    "From: " + doc.data().from
+                newcard.querySelector('.card-length').innerHTML =
+                    "To: " + doc.data().to
+
+                currentUser.get().then(userDoc => {
+
+        })
+
+                //attach to gallery, Example: "hikes-go-here"
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
