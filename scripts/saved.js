82fb77003a29d432441c97b167e8c39b809fa2c7
Davin Higo <davinhigo@gmail.com>
2023-12-06 18:00:16 -0800

    added more features to bookmarked rides, edited button cards in main

--- scripts/saved.js
+++ scripts/saved.js
@@ -2,15 +2,34 @@
 // This function is the only function that's called.
 // This strategy gives us better control of the page.
 //----------------------------------------------------------
+var currentUser
+
+
 function doAll() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
+            currentUser = db.collection("users").doc(user.uid); //global
             insertNameFromFirestore(user);
             getBookmarks(user)
         } else {
             console.log("No user is signed in");
         }
     });
+
+    currentUser.get().then(userDoc => {
+                    let bookmarks = userDoc.data().bookmarks;
+                    if (bookmarks == undefined) {
+                        currentUser.update({
+                            bookmarks: firebase.firestore.FieldValue.arrayUnion("none")
+                        })
+                    }
+
+                    if (bookmarks.includes(docID)) {
+                        document.getElementById('save-' + docID).innerText = 'bookmark';
+                    } else {
+                        document.getElementById('save-' + docID).innerText = 'bookmark_border';
+                    }
+                });
 }
 doAll();
 
@@ -24,7 +43,6 @@ function insertNameFromFirestore(user) {
                 console.log(userDoc.data().name)
                 userName = userDoc.data().name;
                 console.log(userName)
-                document.getElementById("name-goes-here").innerHTML = userName;
             })
 
 }
@@ -39,34 +57,79 @@ function getBookmarks(user) {
         .then(userDoc => {
 
 					  // Get the Array of bookmarks
-            var bookmarks = userDoc.data().bookmarks;
-            console.log(bookmarks);
-						
+            var bookmarks = userDoc.data().bookmarks;				
 						// Get pointer the new card template
-            let newcardTemplate = document.getElementById("savedCardTemplate");
+            let cardTemplate = document.getElementById("savedCardTemplate");
+            let newcard = cardTemplate.content.cloneNode(true);
 
 						// Iterate through the ARRAY of bookmarked hikes (document ID's)
-            bookmarks.forEach(thisHikeID => {
-                console.log(thisHikeID);
-                db.collection("posts").doc(thisHikeID).get().then(doc => {
-                    var poster = doc.data().posterName; 
+            bookmarks.forEach(rideID => {
+                console.log(rideID);
+                db.collection("posts").doc(rideID).get().then(doc => {
+                    
                     var from = doc.data().from;  // get value of the "details" key
 				    var to = doc.data().to;    //get unique ID to each hike to be used for fetching right image
                     var price = doc.data().price; //gets the length field
                     var docID = doc.id;
                     var posterID = doc.data().posterUID;
-                    let newcard = newcardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
-
+                    var dt = doc.data().date + " " + doc.data().time;
+                    var poster = doc.data().posterName; 
 
                     //update title and some pertinant information
                     //update title and text and image
                     newcard.querySelector('.card-title').innerHTML = poster;
-                    newcard.querySelector('.card-length').innerHTML = from;
+                newcard.querySelector('.card-text').innerHTML = "From: " + from;
+                newcard.querySelector('.card-text2').innerHTML = "To: " + to;
+                newcard.querySelector('a').href = "users.html?docID=" + posterID;
+                newcard.querySelector('.card-href').href = "eachpostsaved.html?docID="+docID;
+                newcard.querySelector('i').id = 'save-' + docID;   //guaranteed to be unique
+                newcard.querySelector('i').onclick = () => updateBookmark(docID), console.log(docID);;
+                newcard.querySelector('.card-length').innerHTML = dt;
                     
-
+                currentUser.get().then(userDoc => {
+                        //get the user name
+                        var bookmarks = userDoc.data().bookmarks;
+                        if (bookmarks == undefined) {
+                            currentUser.update({
+                                bookmarks: firebase.firestore.FieldValue.arrayUnion("none")
+                            })
+                        }
+                        if (bookmarks.includes(docID)) {
+                           document.getElementById('save-' + docID).innerText = 'bookmark';
+                        } else {
+                        document.getElementById('save-' + docID).innerText = 'bookmark_border';
+                        }
+                  })
 										//Finally, attach this new card to the gallery
-                    hikeCardGroup.appendChild(newcard);
+                    document.getElementById("saved-go-here").appendChild(newcard);
                 })
             });
         })
 }
+
+function updateBookmark(hikeDocID) {
+    currentUser.get().then(userDoc => {
+        let bookmarks = userDoc.data().bookmarks;
+        let iconID = 'save-' + hikeDocID;
+        let isBookmarked = bookmarks.includes(hikeDocID);
+
+        if (isBookmarked) {
+            // Remove bookmark
+            currentUser.update({
+                bookmarks: firebase.firestore.FieldValue.arrayRemove(hikeDocID)
+            }).then(() => {
+                console.log("Bookmark removed for " + hikeDocID);
+                document.getElementById(iconID).innerText = 'bookmark_border';
+            });
+        } else {
+            // Add bookmark
+            currentUser.update({
+                bookmarks: firebase.firestore.FieldValue.arrayUnion(hikeDocID)
+            }).then(() => {
+                console.log("Bookmark added for " + hikeDocID);
+                document.getElementById(iconID).innerText = 'bookmark';
+            });
+        }
+    });
+}
+
