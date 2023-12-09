3045d5146de2fad1b1e38d0e7d0a024b564d8b5f
Declan Joyce <declan.daniel.joyce@gmail.com>
2023-12-08 18:13:02 -0800

    Changing scripts files and updating readme file

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,22 +1,26 @@
-var currentUser;               //points to the document of the user who is logged in
+var currentUser;   
+
+//---------------------------------------------------
+// Retrieve and fill our user info for your profile.
+//---------------------------------------------------
 function populateUserInfo() {
             firebase.auth().onAuthStateChanged(user => {
-                // Check if user is signed in:
+               
                 if (user) {
 
-                    //go to the correct user document by referencing to the user uid
+                    
                     currentUser = db.collection("users").doc(user.uid)
-                    //get the document for current user.
+                 
                     currentUser.get()
                         .then(userDoc => {
-                            //get the data fields of the user
+                            
                             var userName = userDoc.data().name;
                             var userDescription = userDoc.data().description;
                             var userCar = userDoc.data().car;
                             var userPhone = userDoc.data().phone;
                             var userEmail = userDoc.data().email;
 
-                            //if the data fields are not empty, then write them in to the form.
+                            
                             if (userName != null) {
                                 document.getElementById("nameInput").value = userName;
                             }
@@ -42,25 +46,29 @@ function populateUserInfo() {
             });
         }
 
-//call the function to run it 
+
 populateUserInfo();
 
+//---------------------------------------------------
+// Allows user fields to be edittable.
+//---------------------------------------------------
 function editUserInfo() {
-   //Enable the form fields
+  
    document.getElementById('personalInfoFields').disabled = false;
 }
 
+//-----------------------------------------------------------------------
+// Saves the information in user profile and sets it to unable to change.
+//-----------------------------------------------------------------------
 function saveUserInfo() {
-     //enter code here
-
-    //a) get user entered values
-    userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
+    
+    userName = document.getElementById('nameInput').value;   
     userDescription = document.getElementById('descriptionInput').value;    
     userCar = document.getElementById('carInput').value; 
     userPhone = document.getElementById('phoneInput').value; 
     userEmail = document.getElementById('emailInput').value;
 
-    //b) update user's document in Firestore
+  
     currentUser.update({
                     name: userName,
                     description: userDescription,
@@ -75,16 +83,18 @@ function saveUserInfo() {
     document.getElementById('personalInfoFields').disabled = true;
 }
 
-
+//---------------------------------------------------
+// Shows the information on each review page.
+//---------------------------------------------------
 function populateReviews() {
     console.log("test");
     let rideCardTemplate = document.getElementById("reviewCardTemplate");
     let rideCardGroup = document.getElementById("reviewCardGroup");
 
-    let params = new URL(window.location.href); // Get the URL from the search bar
+    let params = new URL(window.location.href);
     let rideID = params.searchParams.get("docID");
 
-    // Double-check: is your collection called "Reviews" or "reviews"?
+    
     db.collection("reviews")
         .where("rideDocID", "==", rideID)
         .get()
@@ -99,7 +109,7 @@ function populateReviews() {
                 var clean = doc.data().clean;
                 var quick = doc.data().quick;
                 var time = doc.data().timestamp.toDate();
-                var rating = doc.data().rating; // Get the rating value
+                var rating = doc.data().rating; 
                 console.log(rating)
 
                 console.log(time);
@@ -115,15 +125,13 @@ function populateReviews() {
                 reviewCard.querySelector(".quick").innerHTML = `Quick: ${quick}`;
                 reviewCard.querySelector( ".description").innerHTML = `Description: ${description}`;
 
-                // Populate the star rating based on the rating value
-                
-	              // Initialize an empty string to store the star rating HTML
+               
 								let starRating = "";
-								// This loop runs from i=0 to i<rating, where 'rating' is a variable holding the rating value.
+								
                 for (let i = 0; i < rating; i++) {
                     starRating += '<span class="material-icons">star</span>';
                 }
-								// After the first loop, this second loop runs from i=rating to i<5.
+								
                 for (let i = rating; i < 5; i++) {
                     starRating += '<span class="material-icons">star_outline</span>';
                 }
@@ -136,8 +144,11 @@ function populateReviews() {
 
 populateReviews();
 
+//---------------------------------------------------
+// Saves and redirects user in review. 
+//---------------------------------------------------
 function saveRideDocumentIDAndRedirect(){
-    let params = new URL(window.location.href) //get the url from the search bar
+    let params = new URL(window.location.href) 
     let ID = params.searchParams.get("docID");
     localStorage.setItem('rideDocID', ID);
     window.location.href = 'review.html';
